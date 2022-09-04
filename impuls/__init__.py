d66a768cc6eae3add9c03aed7cbc11ceae2b2e8c
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-09-04 18:46:00 +0900

    Make most-used objects available via impuls.*

--- impuls/__init__.py
+++ impuls/__init__.py
@@ -1,1 +1,7 @@
+from . import model
+from .db import DBConnection
+from .pipeline import Pipeline, PipelineOptions, Task
+from .resource import HTTPResource, LocalResource, Resource
+from .tools.logs import initialize as initialize_logging
+
 __version__ = '0.1.0'
