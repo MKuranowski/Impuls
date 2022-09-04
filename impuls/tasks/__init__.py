5906dbc8da006bfa21c73f867bc04db87e83d432
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-09-04 18:57:15 +0900

    Simple task to add a single entity to the database

---
+++ impuls/tasks/__init__.py
@@ -0,0 +1,1 @@
+from .add_entity import AddEntity
---
+++ impuls/tasks/add_entity.py
@@ -0,0 +1,20 @@
+import logging
+
+from .. import model
+from ..db import DBConnection
+from ..pipeline import Task, PipelineOptions
+
+
+class AddEntity(Task):
+    """AddEntity is a simple task that adds the provided entity to the DB."""
+    entity: model.ImpulsBase
+    name: str
+    logger: logging.Logger
+
+    def __init__(self, entity: model.ImpulsBase) -> None:
+        self.entity = entity
+        self.name = "AddAgency"
+        self.logger = logging.getLogger(f"Task.{self.name}")
+
+    def execute(self, db: DBConnection, options: PipelineOptions) -> None:
+        db.save(self.entity)
