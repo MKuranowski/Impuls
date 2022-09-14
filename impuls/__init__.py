45f029de0d2324e67dfffa9838cb4868327395de
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-09-14 14:05:14 +0900

    More useful top-level imports

--- impuls/__init__.py
+++ impuls/__init__.py
@@ -1,7 +1,7 @@
-from . import model
+from . import errors, model, tasks
 from .db import DBConnection
 from .pipeline import Pipeline, PipelineOptions, Task
-from .resource import HTTPResource, LocalResource, Resource
+from .resource import HTTPResource, LocalResource, Resource, ResourceManager
 from .tools.logs import initialize as initialize_logging
 
 __version__ = '0.1.0'
