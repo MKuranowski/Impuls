2120b9dfddcdda469b520b0492fdc3cc7526f2cf
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-12-30 16:51:19 +0900

    Test logs.ColoredFormatter

--- tests/tools/test_logs.py
+++ tests/tools/test_logs.py
@@ -1,11 +1,12 @@
 import logging
+import re
 import sys
 import unittest
 from io import StringIO
 from typing import TYPE_CHECKING
 from unittest.mock import patch
 
-from impuls.tools import logs
+from impuls.tools import color, logs
 
 
 class TestInitializeLogging(unittest.TestCase):
@@ -82,4 +83,66 @@ class TestInitializeLogging(unittest.TestCase):
             handler_retained_1,
             self.fake_root_logger.handlers,
             "NullHandler() should not have been removed by logs.initialize",
+        )
+
+
+class TestColoredFormatter(unittest.TestCase):
+    LOGGER_NAME = "SomeLogger"
+
+    def setUp(self) -> None:
+        self.output = StringIO()
+        self.logger = logging.Logger(self.LOGGER_NAME)
+        handler = logging.StreamHandler(self.output)
+        handler.setFormatter(logs.ColoredFormatter())
+        self.logger.addHandler(handler)
+
+    def expected_msg_format(self, level: str, msg_color: str, msg: str) -> str:
+        blue_e = re.escape(color.BLUE)
+        cyan_e = re.escape(color.CYAN)
+        green_e = re.escape(color.GREEN)
+        reset_e = re.escape(color.RESET)
+
+        logger_e = re.escape(self.LOGGER_NAME)
+        level_e = re.escape(level)
+        msg_color_e = re.escape(msg_color)
+        msg_e = re.escape(msg)
+
+        return (
+            rf"{blue_e}\[{cyan_e}{level_e}{blue_e} \d\d?:\d\d:\d\d\.\d\d\d] "
+            rf"{green_e}{logger_e}{reset_e}: {msg_color_e}{msg_e}{reset_e}"
+        )
+
+    def test_debug(self) -> None:
+        self.logger.debug("Hello world")
+        self.assertRegex(
+            self.output.getvalue(),
+            self.expected_msg_format("DEBUG", color.DIM, "Hello world"),
+        )
+
+    def test_info(self) -> None:
+        self.logger.info("Hello world")
+        self.assertRegex(
+            self.output.getvalue(),
+            self.expected_msg_format("INFO", color.RESET, "Hello world"),
+        )
+
+    def test_warning(self) -> None:
+        self.logger.warning("Hello world")
+        self.assertRegex(
+            self.output.getvalue(),
+            self.expected_msg_format("WARNING", color.YELLOW, "Hello world"),
+        )
+
+    def test_error(self) -> None:
+        self.logger.error("Hello world")
+        self.assertRegex(
+            self.output.getvalue(),
+            self.expected_msg_format("ERROR", color.RED, "Hello world"),
+        )
+
+    def test_critical(self) -> None:
+        self.logger.critical("Hello world")
+        self.assertRegex(
+            self.output.getvalue(),
+            self.expected_msg_format("CRITICAL", color.WHITE + color.BG_RED, "Hello world"),
         )
