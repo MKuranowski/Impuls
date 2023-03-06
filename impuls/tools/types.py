00898d7dcd09c617fa75be5fe907582444e6da63
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-03-06 13:41:31 +0900

    Move union_to_tuple_of_types to types module

--- impuls/tools/types.py
+++ impuls/tools/types.py
@@ -1,9 +1,20 @@
-from typing import TYPE_CHECKING, TypeVar
+import sys
+import typing
+from typing import TYPE_CHECKING, Type, TypeVar, Union
 
 if TYPE_CHECKING:
     from typing_extensions import Self
 else:
     Self = TypeVar("Self")
+
+
+if sys.version_info >= (3, 10):
+    from types import UnionType
+else:
+    # Use a sentinel object instead of UnionType, as it does not exist before 3.10
+    # The sentinel object is used so that `is` with any other object returns False.
+    UnionType = object()
+
 
 SQLNativeType = None | int | float | str
 T = TypeVar("T")
@@ -11,3 +22,22 @@ T = TypeVar("T")
 
 def identity(x: T) -> T:
     return x
+
+
+def union_to_tuple_of_types(tp: Type[T]) -> tuple[Type[T], ...]:
+    """union_to_tuple_of_types tp if it's a non-type-hint type,
+    the type arguments of Union (and therefore also Optional) and UnionType;
+    and raises TypeError for any other type hints.
+
+    >>> _union_to_tuple_of_types(int)
+    (<class 'int'>,)
+    >>> _union_to_tuple_of_types(Union[int, str])
+    (<class 'int'>, <class 'str'>)
+    """
+    origin = typing.get_origin(tp)
+    if origin is None:
+        return (tp,)
+    elif origin is Union or origin is UnionType:
+        return typing.get_args(tp)
+    else:
+        raise TypeError(f"{tp} is neither a non-type-hint type, nor a union")
