8a6996c93dcef035986515fe965af023dfe911b2
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-04-04 11:34:37 +0900

    Complete DB tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,11 +4,12 @@ from pathlib import Path
 from typing import cast
 
 from impuls.db import DBConnection, EmptyQueryResult
+from impuls.model import Agency, Route
 
 FIXTURES = Path(__file__).parent / "fixtures"
 
 
-class TestDBConnection(unittest.TestCase):
+class TestWithoutModel(unittest.TestCase):
     def test_context_manager(self) -> None:
         with DBConnection(path=":memory:") as db:
             self.assertEqual(db.raw_execute("SELECT 0;").one(), (0,))
@@ -105,10 +106,125 @@ class TestDBConnection(unittest.TestCase):
             )
 
 
-class TestDBConnectionWithModel(unittest.TestCase):
-    def test_create_schema(self) -> None:
+class TestCreatesSchema(unittest.TestCase):
+    def test(self) -> None:
         with DBConnection.create_with_schema(path=":memory:"):
             pass
+
+
+class TestWithModel(unittest.TestCase):
+    def setUp(self) -> None:
+        self.db = DBConnection(":memory:")
+        with sqlite3.Connection(FIXTURES / "wkd.db") as con:
+            con.backup(self.db._con)
+
+    def tearDown(self) -> None:
+        self.db.close()
+
+    def test_raw_execute(self) -> None:
+        self.assertEqual(self.db.raw_execute("SELECT COUNT(*) FROM routes;").one(), (3,))
+
+    def assertAgencies(self, *agencies: str) -> None:
+        self.assertSetEqual(
+            set(cast(str, i[0]) for i in self.db.raw_execute("SELECT name FROM agencies;")),
+            set(agencies),
+        )
+
+    def assertRoutes(self, *short_names: str) -> None:
+        self.assertSetEqual(
+            set(cast(str, i[0]) for i in self.db.raw_execute("SELECT short_name FROM routes;")),
+            set(short_names),
+        )
+
+    def test_raw_execute_many(self) -> None:
+        self.assertAgencies("Warszawska Kolej Dojazdowa")
+
+        self.db.raw_execute_many(
+            "INSERT INTO agencies (agency_id, name, url, timezone) VALUES (?, ?, ?, ?);",
+            [
+                ("1", "Foo", "https://example.com/", "UTC"),
+                ("2", "Bar", "https://example.com/", "UTC"),
+                ("3", "Baz", "https://example.com/", "UTC"),
+            ],
+        )
+
+        self.assertAgencies("Warszawska Kolej Dojazdowa", "Foo", "Bar", "Baz")
+
+    def test_typed_in_execute(self) -> None:
+        self.assertAgencies("Warszawska Kolej Dojazdowa")
+        self.db.typed_in_execute(
+            "INSERT INTO :table VALUES :vals",
+            Agency("1", "Foo", "https://example.com/", "UTC"),
+        )
+        self.assertAgencies("Warszawska Kolej Dojazdowa", "Foo")
+
+    def test_typed_in_execute_many(self) -> None:
+        self.assertAgencies("Warszawska Kolej Dojazdowa")
+        self.db.typed_in_execute_many(
+            "INSERT INTO :table VALUES :vals",
+            Agency,
+            [
+                Agency("1", "Foo", "https://example.com/", "UTC"),
+                Agency("2", "Bar", "https://example.com/", "UTC"),
+            ],
+        )
+        self.assertAgencies("Warszawska Kolej Dojazdowa", "Foo", "Bar")
+
+    def test_typed_out_execute(self) -> None:
+        routes = self.db.typed_out_execute(
+            "SELECT * FROM :table WHERE type = ?;",
+            Route,
+            (3,),
+        ).all()
+        self.assertSetEqual(set(i.short_name for i in routes), {"ZA1", "ZA12"})
+
+    def test_retrieve(self) -> None:
+        self.assertEqual(
+            getattr(self.db.retrieve(Agency, "0"), "name", None),
+            "Warszawska Kolej Dojazdowa",
+        )
+        self.assertIsNone(self.db.retrieve(Agency, "Missing"))
+
+    def test_retrieve_must(self) -> None:
+        self.assertEqual(
+            self.db.retrieve_must(Agency, "0").name,
+            "Warszawska Kolej Dojazdowa",
+        )
+
+        with self.assertRaises(EmptyQueryResult):
+            self.db.retrieve_must(Agency, "Missing")
+
+    def test_create(self) -> None:
+        self.assertAgencies("Warszawska Kolej Dojazdowa")
+        self.db.create(Agency("1", "Foo", "https://example.com/", "UTC"))
+        self.assertAgencies("Warszawska Kolej Dojazdowa", "Foo")
+
+    def test_create_many(self) -> None:
+        self.assertAgencies("Warszawska Kolej Dojazdowa")
+        self.db.create_many(
+            Agency,
+            [
+                Agency("1", "Foo", "https://example.com/", "UTC"),
+                Agency("2", "Bar", "https://example.com/", "UTC"),
+            ],
+        )
+        self.assertAgencies("Warszawska Kolej Dojazdowa", "Foo", "Bar")
+
+    def test_update(self) -> None:
+        self.assertAgencies("Warszawska Kolej Dojazdowa")
+        self.db.update(Agency("0", "WKD", "https://wkd.com.pl", "Europe/Warsaw"))
+        self.assertAgencies("WKD")
+
+    def test_update_many(self) -> None:
+        self.assertRoutes("A1", "ZA1", "ZA12")
+        self.db.update_many(
+            Route,
+            [
+                Route("ZA1", "0", "A1-BUS", "", Route.Type.BUS),
+                Route("ZA12", "0", "A12-BUS", "", Route.Type.BUS),
+            ],
+        )
+        self.assertRoutes("A1", "A1-BUS", "A12-BUS")
 
 
 class TestUntypedQueryResult(unittest.TestCase):
@@ -153,3 +269,49 @@ class TestUntypedQueryResult(unittest.TestCase):
     def test_all(self) -> None:
         with self.db.raw_execute("SELECT route_id FROM routes;") as cur:
             self.assertListEqual(cur.all(), [("A1",), ("ZA1",), ("ZA12",)])
+
+
+class TestTypedQueryResult(unittest.TestCase):
+    def setUp(self) -> None:
+        self.db = DBConnection(":memory:")
+        with sqlite3.Connection(FIXTURES / "wkd.db") as con:
+            con.backup(self.db._con)
+
+    def tearDown(self) -> None:
+        self.db.close()
+
+    def test_context_manager_closes(self) -> None:
+        with self.db.typed_out_execute("SELECT * FROM agencies;", Agency) as cur:
+            pass
+
+        with self.assertRaises(sqlite3.ProgrammingError):
+            cur.one()
+
+    def test_iter(self) -> None:
+        with self.db.typed_out_execute("SELECT * FROM routes;", Route) as cur:
+            routes = list(cur)
+            self.assertTrue(all(isinstance(i, Route) for i in routes))  # type: ignore
+            self.assertListEqual([i.id for i in routes], ["A1", "ZA1", "ZA12"])
+
+    def test_one(self) -> None:
+        with self.db.typed_out_execute("SELECT * FROM agencies;", Agency) as cur:
+            self.assertEqual(getattr(cur.one(), "name", None), "Warszawska Kolej Dojazdowa")
+            self.assertIsNone(cur.one())
+
+    def test_one_must(self) -> None:
+        with self.db.typed_out_execute("SELECT * FROM agencies;", Agency) as cur:
+            self.assertEqual(cur.one_must("first").name, "Warszawska Kolej Dojazdowa")
+            with self.assertRaisesRegex(EmptyQueryResult, r"^second$"):
+                cur.one_must("second")
+
+    def test_many(self) -> None:
+        with self.db.typed_out_execute("SELECT * FROM routes;", Route) as cur:
+            all: list[str] = []
+            while chunk := cur.many():
+                all.extend(i.id for i in chunk)
+
+            self.assertListEqual(all, ["A1", "ZA1", "ZA12"])
+
+    def test_all(self) -> None:
+        with self.db.typed_out_execute("SELECT * FROM routes;", Route) as cur:
+            self.assertListEqual([i.id for i in cur.all()], ["A1", "ZA1", "ZA12"])
