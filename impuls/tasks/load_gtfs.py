2eb1698e2f933b6d3677c6ab594eb120e7a5ae19
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-05-28 15:42:24 +0900

    Fix load_gtfs task

--- impuls/tasks/load_gtfs.py
+++ impuls/tasks/load_gtfs.py
@@ -4,22 +4,26 @@ from io import TextIOWrapper
 from typing import final
 from zipfile import ZipFile
 
-from ... import Task, TaskRuntime, model
+from ..model import (
+    ALL_MODEL_ENTITIES,
+    Agency,
+    Attribution,
+    Calendar,
+    CalendarException,
+    Date,
+    Route,
+)
+from ..task import Task, TaskRuntime
 
 
 @final
 class LoadGTFS(Task):
-    source: str
-    fetch_time: datetime | None
-
-    def __init__(self, source: str) -> None:
+    def __init__(self, resource: str) -> None:
         super().__init__()
-
-        self.source = source
-        self.fetch_time = None
+        self.resource = resource
 
     def execute(self, r: TaskRuntime) -> None:
-        gtfs_path = r.resources.get_resource_path(self.source)
+        gtfs_path = r.resources[self.resource].stored_at
         self.fetch_time = datetime.today()
 
         # Try to import every table
@@ -28,7 +32,7 @@ class LoadGTFS(Task):
         required_tables = {"routes", "stops", "trips", "stop_times"}
 
         with ZipFile(gtfs_path, mode="r") as arch:
-            for typ in model.ALL_MODEL_ENTITIES:
+            for typ in ALL_MODEL_ENTITIES:
                 # Find the table file
                 try:
                     arch.getinfo(typ.gtfs_table_name() + ".txt")
@@ -51,17 +55,17 @@ class LoadGTFS(Task):
                         # Hacks to conform the GTFS model to Impuls model
 
                         # agency_id is required by Impuls, but not GTFS
-                        if typ in (model.Agency, model.Route) and not row.get("agency_id"):
+                        if typ in (Agency, Route) and not row.get("agency_id"):
                             row["agency_id"] = "(missing)"
 
                         # CalendarException can exist without parent Calendar in GTFS,
                         # but not Impuls
                         if (
-                            typ is model.CalendarException
-                            and r.db.retrieve(model.Calendar, row["service_id"]) is None
+                            typ is CalendarException
+                            and r.db.retrieve(Calendar, row["service_id"]) is None
                         ):
                             r.db.create(
-                                model.Calendar(
+                                Calendar(
                                     row["service_id"],
                                     monday=False,
                                     tuesday=False,
@@ -70,13 +74,13 @@ class LoadGTFS(Task):
                                     friday=False,
                                     saturday=False,
                                     sunday=False,
-                                    start_date=model.Date.SIGNALS_EXCEPTIONS,
-                                    end_date=model.Date.SIGNALS_EXCEPTIONS,
+                                    start_date=Date.SIGNALS_EXCEPTIONS,
+                                    end_date=Date.SIGNALS_EXCEPTIONS,
                                 )
                             )
 
                         # Attribution has to have an id
-                        if typ is model.Attribution and not row.get("attribution_id"):
+                        if typ is Attribution and not row.get("attribution_id"):
                             row["attribution_id"] = str(reader.line_num)
 
                         # Persist the entity
