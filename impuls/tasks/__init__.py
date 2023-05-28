cfbb015a61cac4e67c3a47d4cde217ff63fd31b9
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-05-28 15:27:33 +0900

    Move load tasks to impuls.tasks

--- impuls/tasks/__init__.py
+++ impuls/tasks/__init__.py
@@ -1,14 +1,16 @@
-from . import load
 from .add_entity import AddEntity
 from .exec_sql import ExecuteSQL
 from .generate_trip_headsign import GenerateTripHeadsign
+from .load_busman import LoadBusManMDB
+from .load_gtfs import LoadGTFS
 from .modify_from_csv import ModifyRoutesFromCSV, ModifyStopsFromCSV
 
 __all__ = [
-    "load",
     "AddEntity",
     "ExecuteSQL",
     "GenerateTripHeadsign",
+    "LoadBusManMDB",
+    "LoadGTFS",
     "ModifyRoutesFromCSV",
     "ModifyStopsFromCSV",
 ]
--- impuls/tasks/load/__init__.py
+++
@@ -1,4 +0,0 @@
-from .busman import LoadBusManMDB
-from .gtfs import LoadGTFS
-
-__all__ = ["LoadBusManMDB", "LoadGTFS"]
