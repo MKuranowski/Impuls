c38e64e012c211d267faa34da594c2e2b25db932
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-06-14 10:13:04 +0900

    Template for testing tasks

---
+++ tests/tasks/template_testcase.py
@@ -0,0 +1,77 @@
+import sqlite3
+from pathlib import Path
+from typing import ClassVar, Mapping
+from unittest import TestCase
+
+from impuls import DBConnection, LocalResource, PipelineOptions, TaskRuntime
+from impuls.resource import ManagedResource
+from impuls.tools.testing_mocks import MockFile, MockResource
+
+FIXTURES_DIR = Path(__file__).with_name("fixtures")
+
+
+class AbstractTestTask:
+    # NOTE: Nested classes are necessary to prevent abstract test cases
+    #       from being discovered and run.
+    #       See https://stackoverflow.com/a/50176291.
+
+    class Template(TestCase):
+        db_name: ClassVar[str] = "wkd.db"
+        resources: ClassVar[Mapping[str, MockResource | LocalResource]] = {}
+        options: ClassVar[PipelineOptions] = PipelineOptions()
+
+        workspace: MockFile
+        runtime: TaskRuntime
+
+        def _prepare_db(self) -> DBConnection:
+            # Open the database, as prescribed by the "save_db_in_workspace" option
+            db = DBConnection(
+                str(self.workspace.path / "impuls.db")
+                if self.options.save_db_in_workspace
+                else ":memory:"
+            )
+
+            # Copy fixture contents into the connection
+            with sqlite3.connect(FIXTURES_DIR / self.db_name) as source:
+                source.backup(db._con)
+
+            return db
+
+        def _prepare_resource(
+            self,
+            name: str,
+            resource: MockResource | LocalResource,
+        ) -> ManagedResource:
+            match resource:
+                case LocalResource():
+                    resource.update_last_modified(fake_fetch_time=True)
+                    return ManagedResource(
+                        resource.path, resource.last_modified, resource.fetch_time
+                    )
+
+                case MockResource():
+                    path = self.workspace.path / name
+                    path.write_bytes(resource.content)
+                    return ManagedResource(path, resource.last_modified, resource.fetch_time)
+
+            raise RuntimeError(f"Disallowed Resource type in fixture: {type(resource).__name__}")
+
+        def _prepare_resources(self) -> dict[str, ManagedResource]:
+            return {
+                name: self._prepare_resource(name, resource)
+                for name, resource in self.resources.items()
+            }
+
+        def setUp(self) -> None:
+            super().setUp()
+            self.workspace = MockFile(directory=True)
+            self.runtime = TaskRuntime(
+                db=self._prepare_db(),
+                resources=self._prepare_resources(),
+                options=self.options,
+            )
+
+        def tearDown(self) -> None:
+            super().tearDown()
+            self.runtime.db.close()
+            self.workspace.cleanup()
