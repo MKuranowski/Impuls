20e12dcfec67b78b3afe6542dbff50a7dac52ef8
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-10-04 22:25:54 +0200

    SaveDB task

---
+++ impuls/tasks/save_db.py
@@ -0,0 +1,18 @@
+from typing import final
+from pathlib import Path
+import sqlite3
+
+from ..task import Task, TaskRuntime
+
+
+@final
+class SaveDB(Task):
+    """SaveDB saves the contained data as-is to a database at a provided path"""
+
+    def __init__(self, to: Path) -> None:
+        super().__init__()
+        self.to = to
+
+    def execute(self, r: TaskRuntime) -> None:
+        with sqlite3.connect(self.to) as target:
+            r.db._con.backup(target)
