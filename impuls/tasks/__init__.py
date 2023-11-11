81d82e97f2000556f9db4be237dae976b8c88aea
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-11-11 13:31:50 +0100

    RemoveUnusedEntities task

--- impuls/tasks/__init__.py
+++ impuls/tasks/__init__.py
@@ -4,6 +4,7 @@ from .generate_trip_headsign import GenerateTripHeadsign
 from .load_busman import LoadBusManMDB
 from .load_gtfs import LoadGTFS
 from .modify_from_csv import ModifyRoutesFromCSV, ModifyStopsFromCSV
+from .remove_unused_entities import RemoveUnusedEntities
 from .save_db import SaveDB
 from .save_gtfs import SaveGTFS
 from .truncate_calendars import TruncateCalendars
@@ -16,6 +17,7 @@ __all__ = [
     "LoadGTFS",
     "ModifyRoutesFromCSV",
     "ModifyStopsFromCSV",
+    "RemoveUnusedEntities",
     "SaveDB",
     "SaveGTFS",
     "TruncateCalendars",
---
+++ impuls/tasks/remove_unused_entities.py
@@ -0,0 +1,84 @@
+from typing import final
+
+from ..db import DBConnection
+from ..model import Calendar, CalendarException
+from ..task import Task, TaskRuntime
+
+
+@final
+class RemoveUnusedEntities(Task):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def execute(self, r: TaskRuntime) -> None:
+        self.drop_trips_with_at_most_one_stop(r.db)
+        self.drop_calendars_without_trips(r.db)
+        self.drop_calendars_without_dates(r.db)
+        self.drop_stops_without_stop_times(r.db)
+        self.drop_stations_without_stops(r.db)
+        self.drop_routes_without_trips(r.db)
+        self.drop_agencies_without_routes(r.db)
+
+    def drop_trips_with_at_most_one_stop(self, db: DBConnection) -> None:
+        result = db.raw_execute(
+            "DELETE FROM trips WHERE "
+            "(SELECT COUNT(*) FROM stop_times WHERE stop_times.trip_id = trips.trip_id) <= 1"
+        )
+        self.logger.info("Dropped %d trip(s) with 0 or 1 stop_time", result.rowcount)
+
+    def drop_calendars_without_trips(self, db: DBConnection) -> None:
+        result = db.raw_execute(
+            "DELETE FROM calendars WHERE NOT EXISTS "
+            "(SELECT trip_id FROM trips WHERE trips.calendar_id = calendars.calendar_id)"
+        )
+        self.logger.info("Dropped %d calendar(s) without any trips", result.rowcount)
+
+    def drop_calendars_without_dates(self, db: DBConnection) -> None:
+        calendars_without_dates = list[tuple[str]]()
+
+        for calendar in db.retrieve_all(Calendar):
+            active_dates = CalendarException.reflect_in_active_dates(
+                calendar.compute_active_dates(),
+                db.typed_out_execute(
+                    "SELECT * FROM :table WHERE calendar_id = ?",
+                    CalendarException,
+                    (calendar.id,),
+                ),
+            )
+            if not active_dates:
+                calendars_without_dates.append((calendar.id,))
+
+        result = db.raw_execute_many(
+            "DELETE FROM calendars WHERE calendar_id = ?",
+            calendars_without_dates,
+        )
+        self.logger.info("Dropped %d calendar(s) without any dates", result.rowcount)
+
+    def drop_stops_without_stop_times(self, db: DBConnection) -> None:
+        result = db.raw_execute(
+            "DELETE FROM stops WHERE location_type = 0 AND "
+            "NOT EXISTS (SELECT stop_id FROM stop_times WHERE stop_times.stop_id = stops.stop_id)"
+        )
+        self.logger.info("Dropped %d stop(s) without any stop times", result.rowcount)
+
+    def drop_stations_without_stops(self, db: DBConnection) -> None:
+        result = db.raw_execute(
+            "DELETE FROM stops AS s1 WHERE s1.location_type = 1 AND NOT EXISTS "
+            "(SELECT stop_id FROM stops s2 WHERE "
+            " s2.location_type = 0 AND s2.parent_station = s1.stop_id)"
+        )
+        self.logger.info("Dropped %d station(s) without any platforms", result.rowcount)
+
+    def drop_routes_without_trips(self, db: DBConnection) -> None:
+        result = db.raw_execute(
+            "DELETE FROM routes WHERE NOT EXISTS "
+            "(SELECT trip_id FROM trips WHERE trips.route_id = routes.route_id)"
+        )
+        self.logger.info("Dropped %d calendar(s) without any trips", result.rowcount)
+
+    def drop_agencies_without_routes(self, db: DBConnection) -> None:
+        result = db.raw_execute(
+            "DELETE FROM agencies WHERE NOT EXISTS "
+            "(SELECT route_id FROM routes WHERE routes.agency_id = agencies.agency_id)"
+        )
+        self.logger.info("Dropped %d calendar(s) without any trips", result.rowcount)
