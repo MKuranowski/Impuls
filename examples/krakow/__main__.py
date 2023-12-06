886d6d3b09caa00931676bfcb68db510d15a47e3
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-12-06 16:27:27 +0100

    Kraków: finish example

--- examples/krakow/__main__.py
+++ examples/krakow/__main__.py
@@ -2,7 +2,9 @@ from argparse import ArgumentParser
 from pathlib import Path
 
 from impuls import HTTPResource, Pipeline, PipelineOptions, initialize_logging
-from impuls.tasks import ExecuteSQL, LoadGTFS
+from impuls.tasks import ExecuteSQL, LoadGTFS, RemoveUnusedEntities
+
+from .generate_route_long_name import GenerateRouteLongName
 
 arg_parser = ArgumentParser()
 arg_parser.add_argument("type", choices=["bus", "tram"])
@@ -25,7 +27,60 @@ Pipeline(
     ),
     tasks=[
         LoadGTFS(source_name),
-        ExecuteSQL(statement="UPDATE trips SET block_id = NULL", task_name="DropBlockID"),
+        ExecuteSQL(task_name="DropBlockID", statement="UPDATE trips SET block_id = NULL"),
+        ExecuteSQL(
+            task_name="RemoveTripsWithoutPickup",
+            statement=(
+                "DELETE FROM trips WHERE NOT EXISTS (SELECT * FROM stop_times WHERE"
+                "  trips.trip_id = stop_times.trip_id AND pickup_type != 1)"
+            ),
+        ),
+        RemoveUnusedEntities(),
+        ExecuteSQL(
+            task_name="FixAgency",
+            statement=(
+                "UPDATE agencies SET name = CASE "
+                "  WHEN url LIKE '%mpk.krakow.pl%' THEN 'MPK Kraków' "
+                "  WHEN url LIKE '%ztp.krakow.pl%' THEN 'ZTP Kraków' "
+                "  ELSE name "
+                "END"
+            ),
+        ),
+        ExecuteSQL(
+            task_name="FixStopNames",
+            statement=r"UPDATE stops SET name = re_sub('(\w)\.(\w)', '\1. \2', name)",
+        ),
+        ExecuteSQL(
+            task_name="FixTripHeadsign",
+            statement=(
+                "UPDATE trips SET headsign = "
+                r"re_sub(' *\(n[zż]\)$', '', re_sub('(\w)\.(\w)', '\1. \2', headsign))"
+            ),
+        ),
+        ExecuteSQL(
+            task_name="FixRouteColor",
+            statement=(
+                "UPDATE routes SET text_color = 'FFFFFF', color ="
+                "  CASE type"
+                "    WHEN 0 THEN '002E5F'"
+                "    ELSE '0072AA'"
+                "  END"
+            ),
+        ),
+        ExecuteSQL(
+            task_name="GenerateStopCode",
+            statement=(
+                "UPDATE stops SET code ="
+                "  CASE"
+                # Tram stops: last 2 digits 'x9' map to 0x
+                "    WHEN substr(stop_id, -2, 2) GLOB '[1-9]9' THEN '0' || substr(stop_id, -2, 1)"
+                # Default: last two digits of the stop_id are the stop_code
+                "    WHEN substr(stop_id, -2, 2) GLOB '[0-9][0-9]' THEN substr(stop_id, -2, 2)"
+                "    ELSE ''"
+                "  END"
+            ),
+        ),
+        GenerateRouteLongName(),
     ],
     resources={
         source_name: HTTPResource.get(source_url),
