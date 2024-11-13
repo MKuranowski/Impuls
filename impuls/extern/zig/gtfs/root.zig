721f1e7fcaf6eccfec2ecb6a74ffdeb34a78c330
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-11-13 14:21:05 +0100

    extern: remove usingnamespace

--- impuls/extern/zig/gtfs/root.zig
+++ impuls/extern/zig/gtfs/root.zig
@@ -1,5 +1,12 @@
 // © Copyright 2022-2024 Mikołaj Kuranowski
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pub usingnamespace @import("load.zig");
-pub usingnamespace @import("save.zig");
+const mod_load = @import("load.zig");
+pub const load = mod_load.load;
+
+const mod_save = @import("save.zig");
+pub const c_char_p = mod_save.c_char_p;
+pub const c_char_p_p = mod_save.c_char_p_p;
+pub const ExtraFile = mod_save.ExtraFile;
+pub const Headers = mod_save.Headers;
+pub const save = mod_save.save;
