c1a4f5faf58efc65813ec46fc5dc4ab31bf9ada4
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-01-07 09:36:42 +0100

    2.0.0 release

--- impuls/__init__.py
+++ impuls/__init__.py
@@ -31,9 +31,9 @@ __title__ = "Impuls"
 __description__ = "Framework for processing static public transportation data"
 __url__ = "https://github.com/MKuranowski/Impuls"
 __author__ = "Mikołaj Kuranowski"
-__copyright__ = "© Copyright 2022-2024 Mikołaj Kuranowski"
+__copyright__ = "© Copyright 2022-2025 Mikołaj Kuranowski"
 __license__ = "GPL-3.0-or-later"
-__version__ = "1.1.2"
+__version__ = "2.0.0"
 __email__ = "mkuranowski+pypackages@gmail.com"
 
 DBConnection = db.DBConnection
